1d9e7c51804c5a817c0cd634c9812d6fca134287
Practice Golang <practice.golang@gmail.com>
2022-05-03 18:12:01 +0900

    use gg

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main // import "hello-ebiten"
 
 import (
+	"image"
+	"image/color"
 	"log"
 
+	"github.com/fogleman/gg"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	eb "github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 type Game struct{}
@@ -13,19 +16,45 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+func drawCircle(x, y, r int) image.Image {
+	dc := gg.NewContext(int(x+(r*2)), int(y+(r*2)))
+
+	dc.Push()
+	dc.SetRGBA(1, 0.5, 0, 1)
+	dc.SetLineWidth(1)
+	dc.DrawCircle(float64(r), float64(r), float64(r))
+	dc.Fill()
+	dc.Pop()
+
+	return dc.Image()
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0x11, 0x22, 0x33, 0xff})
+
+	eb.DrawLine(screen, -50, -50, 50, 50, color.White)
+	eb.DrawRect(screen, 150, 150, 50, 50, color.Black)
+	eb.DebugPrint(screen, "Hello, World!")
+
+	m := drawCircle(100, 100, 50)
+	em := ebiten.NewImageFromImage(m)
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(0, 0)
+	opts.GeoM.Scale(1.5, 1)
+	screen.DrawImage(em, opts)
+}
+
+func (g *Game) Layout(outsideW, outsideH int) (screenW, screenH int) {
 	return 320, 240
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!!")
+	app := &Game{}
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, Ebiten!!")
+
+	if err := ebiten.RunGame(app); err != nil {
 		log.Fatal(err)
 	}
 }
